61b35fc14ebd3dc8531fc7b92e9051ecc64874a2
Redd <35120080+reddcaruana@users.noreply.github.com>
2025-01-26 13:19:03 +0100

    Updates
    Changes:
    
    + Potions
    + Leaderboard Script
    + Shipping box setup

---
+++ Assets/GlobalGameJam/Scripts/Scoring/SessionOutcome.cs
@@ -0,0 +1,10 @@
+namespace GlobalGameJam
+{
+    public struct SessionOutcome
+    {
+        public string Name;
+        public int Score;
+        public int Time;
+        public bool Highlight;
+    }
+}
